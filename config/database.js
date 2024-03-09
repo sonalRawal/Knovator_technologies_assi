a1e202d23d4fecab160b53a52eb1247f35f16fa1
Sonal Rawal <sonu@codeops.tech>
2024-03-09 14:46:42 +0530

    minor changes

--- config/database.js
+++ config/database.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/nodejs_assignment', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+mongoose.connect('mongodb://localhost:27017/nodejsAssignment', {
+  useNewUrlParser: true
 })
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Connection to MongoDB failed:', err));
